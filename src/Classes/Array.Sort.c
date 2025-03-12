b3af42875286f695e46b221483b2d7f730a6e864
0w0Demonic <paulroemer14.05@gmail.com>
2025-03-12 10:49:45 +0100

    Add Array.Sort() tests and code cleaning

--- src/Classes/Array.Sort.c
+++ src/Classes/Array.Sort.c
@@ -1,4 +1,4 @@
-// cl.exe /O2 /favor:AMD64 /LD Array.Sort.c ole32.lib oleaut32.lib
+// args: cl.exe /O2 /favor:AMD64 /LD Array.Sort.c ole32.lib oleaut32.lib
 
 #include <windows.h>
 #include <oleauto.h>
@@ -7,16 +7,33 @@
 
 typedef int (*ComparatorFunc)(VARIANT*, VARIANT*);
 
-void QuickSort(IDispatch*, IDispatchVtbl*, DISPID, DISPID, int, int, ComparatorFunc);
-int Partition(IDispatch*, IDispatchVtbl*, DISPID, DISPID, int, int, ComparatorFunc);
-void SwapElements(IDispatch*, IDispatchVtbl*, DISPID, DISPID, int, int);
+void QuickSort(
+    IDispatch*, IDispatchVtbl*,
+    DISPID, DISPID,
+    int, int,
+    ComparatorFunc
+);
+
+int Partition(
+    IDispatch*, IDispatchVtbl*,
+    DISPID, DISPID,
+    int, int,
+    ComparatorFunc
+);
+
+void SwapElements(
+    IDispatch*, IDispatchVtbl*,
+    DISPID, DISPID,
+    int, int
+);
+
 HRESULT InvokeGet(IDispatch*, IDispatchVtbl*, DISPID, int, VARIANT*);
 HRESULT InvokeSet(IDispatch*, IDispatchVtbl*, DISPID, int, VARIANT*);
 
 __declspec(dllexport) int sort(
 	VARIANT* pVariant,
 	DWORD dwLen,
-	ComparatorFunc comparator)
+	ComparatorFunc comp)
 {
     if (!pVariant || pVariant->vt != VT_DISPATCH || !pVariant->pdispVal) {
         return -1;
@@ -31,15 +48,15 @@ __declspec(dllexport) int sort(
 
     HRESULT hr;
 	hr = lpVtbl->GetIDsOfNames(pDisp, &IID_NULL, &get, 1, 0, &dispidGet);
-    if (FAILED(hr)) return -2;
-
+    if (FAILED(hr)) {
+        return -2;
+    }
 	hr = lpVtbl->GetIDsOfNames(pDisp, &IID_NULL, &set, 1, 0, &dispidSet);
-    if (FAILED(hr)) return -3;
+    if (FAILED(hr)) {
+        return -3;
+    }
 
-	VARIANT index = { .vt = VT_I4, .lVal = 2 };
-	DISPPARAMS indexArg = { &index, NULL, 1, 0 };
-
-	QuickSort(pDisp, lpVtbl, dispidGet, dispidSet, 0, dwLen - 1, comparator);
+	QuickSort(pDisp, lpVtbl, dispidGet, dispidSet, 0, dwLen - 1, comp);
     return 0;
 }
 
@@ -47,20 +64,22 @@ void QuickSort(
     IDispatch* pDisp, IDispatchVtbl* lpVtbl,
     DISPID dispidGet, DISPID dispidSet,
     int low, int high,
-    ComparatorFunc comparator)
+    ComparatorFunc comp)
 {
-    if (low >= high) return;
+    if (low >= high) {
+        return;
+    }
 
-    int pivotIndex = Partition(pDisp, lpVtbl, dispidGet, dispidSet, low, high, comparator);
-    QuickSort(pDisp, lpVtbl, dispidGet, dispidSet, low, pivotIndex - 1, comparator);
-    QuickSort(pDisp, lpVtbl, dispidGet, dispidSet, pivotIndex + 1, high, comparator);
+    int pivot = Partition(pDisp, lpVtbl, dispidGet, dispidSet, low, high, comp);
+    QuickSort(pDisp, lpVtbl, dispidGet, dispidSet, low, pivot - 1, comp);
+    QuickSort(pDisp, lpVtbl, dispidGet, dispidSet, pivot + 1, high, comp);
 }
 
 int Partition(
     IDispatch* pDisp, IDispatchVtbl* lpVtbl,
     DISPID dispidGet, DISPID dispidSet,
     int low, int high,
-    ComparatorFunc comparator)
+    ComparatorFunc comp)
 {
     VARIANT pivot, temp, v1;
     VariantInit(&pivot);
@@ -71,7 +90,7 @@ int Partition(
     int i = low - 1;
     for (int j = low; j < high; j++) {
         InvokeGet(pDisp, lpVtbl, dispidGet, j, &v1);
-        if (comparator(&v1, &pivot) < 0) {
+        if (comp(&v1, &pivot) < 0) {
             i++;
             SwapElements(pDisp, lpVtbl, dispidGet, dispidSet, i, j);
         }
